a08444f3f60b1b1116da65d454b771f7a429d29a
Brendan Castillo <bomber8184@icloud.com>
2024-06-05 19:36:11 +0800

    Removed type submit

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,4 +1,3 @@
-
 const setError = (element, message) => {
     const inputControl = element.parentElement;
     const errorDisplay = inputControl.querySelector('.error');
@@ -15,17 +14,15 @@ const setSuccess = element => {
     errorDisplay.innerText = '';
     inputControl.classList.add('success');
     inputControl.classList.remove('error');
-
 };
 
 const validateInputs = () => {
-    
-    const first_name = document.getElementById('first-name').value;
-    const last_name = document.getElementById('last-name').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirm-password').value;
-    const mobile_number = document.getElementById('mobile-number').value;
-    const email = document.getElementById('email').value;
+    const first_name = document.getElementById('first-name');
+    const last_name = document.getElementById('last-name');
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('confirm-password');
+    const mobile_number = document.getElementById('mobile-number');
+    const email = document.getElementById('email');
     
     const valid_first_name = first_name.value.trim();
     const valid_last_name = last_name.value.trim();
@@ -34,13 +31,15 @@ const validateInputs = () => {
     const valid_mobile_number = mobile_number.value.trim();
     const valid_email = email.value.trim();
 
+    let isValid = true;
 
     // First Name Validation
     if(valid_first_name === '') {
         setError(first_name, 'First name cannot be blank');
+        isValid = false;
     } else if (!isValidFirstName(valid_first_name)) {
         setError(first_name, "Provide a valid first name");
-
+        isValid = false;
     } else {
          setSuccess(first_name);
     } 
@@ -48,94 +47,91 @@ const validateInputs = () => {
     // Last Name Validation
     if (valid_last_name === '') {
         setError(last_name, 'Last name cannot be blank');
+        isValid = false;
     } else if (!isValidLastName(valid_last_name)){
         setError(last_name, "Provide a valid last name");
-    
+        isValid = false;
     } else {
         setSuccess(last_name);
     }
 
-
-    // Valid Password Validation
+    // Password Validation
     if (valid_password === '') {
         setError(password, 'Password cannot be blank');
+        isValid = false;
     } else if(!isValidPassword(valid_password)){
         setError(password, "Password should contain at least 1 uppercase letter, 1 lowercase letter, 1 number, 1 special character, and must be 12-16 characters long");
-    }
-    else {
+        isValid = false;
+    } else {
         setSuccess(password);
     }
 
-    // Valid Confirm Password Validation
+    // Confirm Password Validation
     if (valid_confirmPassword === '') {
         setError(confirmPassword, 'Confirm Password cannot be blank');
-    }
-    else if (valid_password !== valid_confirmPassword) {
+        isValid = false;
+    } else if (valid_password !== valid_confirmPassword) {
         setError(confirmPassword, 'Passwords do not match');
-    }
-    else {
+        isValid = false;
+    } else {
         setSuccess(confirmPassword);
     }
 
     // Mobile Number Validation
     if (valid_mobile_number === '') {
         setError(mobile_number, 'Mobile number cannot be blank');
-
+        isValid = false;
     } else if(!isValidMobileNumber(valid_mobile_number)){
         setError(mobile_number, "Provide a valid mobile number");
-    }
-    else {
+        isValid = false;
+    } else {
         setSuccess(mobile_number);
     }
 
     // Email Validation
     if (valid_email === '') {
         setError(email, 'Email cannot be blank');
-    }
-    else {
+        isValid = false;
+    } else {
         setSuccess(email);
     }
 
-
+    return isValid;
 };
 
-    
-    const isValidFirstName = first_name => {
-        const re = /^[A-Za-zÑñ  ]{1,30}$/;
-        return re.test(String(first_name));
+const isValidFirstName = first_name => {
+    const re = /^[A-Za-zÑñ  ]{1,30}$/;
+    return re.test(String(first_name));
+};
 
-    };
+const isValidLastName = last_name => {
+    const re = /^[A-Za-zÑñ  ]{1,30}$/;
+    return re.test(String(last_name));
+};
 
-    const isValidLastName = last_name => {
-        const re = /^[A-Za-zÑñ  ]{1,30}$/;
-        return re.test(String(last_name));
-    };
+const isValidPassword = password => {
+    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*_])[A-Za-z\d@$!%*_]{12,16}$/;
+    return re.test(String(password));
+};
 
-    const isValidPassword = password =>{
-        const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*_])[A-Za-z\d@$!%*_]{12,16}$/;
-        return re.test(String(password));
-    };
+const isValidMobileNumber = mobile_number => {
+    const re = /^(\+639\d{9}|^09\d{9})$/;
+    return re.test(String(mobile_number));
+}
 
-    const isValidMobileNumber = mobile_number => {
-        const re = /^(\+639\d{9}|^09\d{9})$/;
-        return re.test(String(mobile_number));
-    }
-
-    const isValidEmail = email =>{
-        const re = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$/;
-        return re.test(String(email));
-    }
-
-
+const isValidEmail = email => {
+    const re = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$/;
+    return re.test(String(email));
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     const registerForm = document.getElementById('registerForm');
     const submitbtn = document.getElementById('submit');
 
-    
     submitbtn.addEventListener('click', (e) => {
         e.preventDefault();
-        validateInputs();
+        if (validateInputs()) {
+            registerForm.submit();
+        }
     });
-    
 });
